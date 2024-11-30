5e8c57e6be5bf0e43ea0de83719a2d80bced06cf
Alejomc26 <alechavezjaja26@gmail.com>
2024-11-30 00:17:22 -0500

    Add project files.

---
+++ include/Chaufa.h
@@ -0,0 +1,8 @@
+﻿// Chaufa.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+void chaufaInitialize();
+
+// TODO: Reference additional headers your program requires here.
---
+++ src/Chaufa.c
@@ -0,0 +1,6 @@
+﻿#include "Chaufa.h"
+#include <stdio.h>
+
+void chaufaInitialize() {
+	printf("hola chaufa\n");
+}
